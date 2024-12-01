d2d94b518b1b48e2a3fd32ad8e0b1042b63f1ebd
Rajan Singh ✅ <145154389+Rajan3208@users.noreply.github.com>
2024-12-01 09:16:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,29 +26,8 @@ def main():
     ### Direct Link
     If the button doesn't work, you can copy and paste this link:
     
-    https://colab.research.google.com/drive/1w1DJXh3wphJXcM-aCUbWvB-1SPmRIYv8#scrollTo=czZZDSdxRcYB&uniqifier=1rajanX
+    https://colab.research.google.com/drive/1w1DJXh3wphJXcM-aCUbWvB-1SPmRIYv8#scrollTo=czZZDSdxRcYB&uniqifier=1
     """)
-        # Create a button that opens the Colab notebook
-    if st.button("Open G Colab Notebook"):
-        st.markdown("""
-        <a href="https://colab.research.google.com/drive/1w1DJXh3wphJXcM-aCUbWvB-1SPmRIYv8#scrollTo=czZZDSdxRcYB&uniqifier=1" 
-           target="_blank" 
-           style="display: inline-block; padding: 10px 20px; background-color: #4CAF50; 
-           color: white; text-decoration: none; border-radius: 5px;">
-           Open Notebook
-        </a>
-        """, unsafe_allow_html=True)
-    
-    # Optional: Display the link as text
-    st.markdown("""
-    ### Direct Link
-    If the button doesn't work, you can copy and paste this link:
-    
-    https://colab.research.google.com/drive/1w1DJXh3wphJXcM-aCUbWvB-1SPmRIYv8#scrollTo=czZZDSdxRcYB&uniqifier=1rajanX
-    """)
-
-
-
 
 if __name__ == "__main__":
     main()
